6fd6f30714842a2cbca80b3464721703941f9f28
JoelSchrage <info@joelschrage.com>
2022-09-09 13:34:28 +0200

    Change function name

--- script.js
+++ script.js
@@ -9,15 +9,13 @@ const loader = document.getElementById('loader');
 let apiQuotes = [];
 
 
-// Show loading
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 };
 
 
-// Hide loading
-function complete() {
+function removeLoadingSpinner() {
     loader.hidden = true;
     quoteContainer.hidden = false;
 };
@@ -25,7 +23,7 @@ function complete() {
 
 // Show new Quote
 function newQuote() {
-    loading();
+    showLoadingSpinner();
 
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
@@ -46,13 +44,13 @@ function newQuote() {
 
     // Set quote, hide loader
     quoteText.textContent = quote.text;
-    complete();
+    removeLoadingSpinner();
 };
 
 
 // Get Quotes from API
 async function getQuotes() {
-    loading();
+    showLoadingSpinner();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
